7ee9310b071bed1e07d05c03340466a535e44f4e
Xharos <devxharos@gmail.com>
2021-02-20 09:01:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# <img src="https://raw.githubusercontent.com/islands-wars/guidelines/master/assets/icon.png" width="64"> Islands Wars - Api
+
+
+> Islands Wars is a Minecraft multiplayer server network.
+
+> This project is a bunch of classes to connect to our various services.
+
+---
+
+# Contributing
+
+Please refer to this [file](https://github.com/islands-wars/guidelines/blob/master/README.md) when contributing to ensure everything is setup correctly.
+Don't forget to use our code style and header, write good commit message and use Java conventions.
+Ensure your code compile, and your tests run.
+
+---
+
+# Wiki
+
+todo
+
+---
+
+# Javadoc
+
+todo
+
+---
+
+# License
+
+---
+
+> GNU GENERAL PUBLIC LICENSE Version 3, see [LICENSE](https://github.com/islands-wars/commons/blob/master/LICENSE) file.
---
+++ src/main/java/fr/islandswars/commons/Hello.java
@@ -0,0 +1,29 @@
+package fr.islandswars.commons;
+
+/**
+ * File <b>Hello</b> located on fr.islandswars.commons
+ * Hello is a part of commons.
+ * <p>
+ * Copyright (c) 2017 - 2021 Islands Wars.
+ * <p>
+ * commons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <a href="http://www.gnu.org/licenses/">GNU license</a>.
+ * <p>
+ *
+ * @author Valentin Burgaud (Xharos), {@literal <xharos@islandswars.fr>}
+ * Created the 20/02/2021 at 08:54
+ * @since 0.2
+ */
+public class Hello {
+
+}

c6c1543befd858e2b3a1b5d82593733b32cf7961
Xharos <devxharos@gmail.com>
2024-05-20 18:20:46 +0200

    Test github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,3 +35,51 @@ tasks.shadowJar {
         attributes["Main-Class"] = "fr.islandswars.commons.Commons"
     }
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            project.shadow.component(this)
+
+            groupId = rootProject.group.toString()
+            artifactId = rootProject.name
+            version = version
+
+            pom {
+                name.set(rootProject.name)
+                description.set("Utility classes for connecting to islands-wars database")
+                url.set("https://github.com/islands-wars/commons")
+
+                licenses {
+                    license {
+                        name.set("The GNU General Public License, Version 3.0")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.html#license-text")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Xharos")
+                        name.set("Burgaud Valentin")
+                        email.set("jangliu@islandswars.fr")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/islands-wars/commons.git")
+                    developerConnection.set("scm:git:ssh://github.com:islands-wars/commons.git")
+                    url.set("https://github.com/islands-wars/commons")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/islands-wars/commons")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+}
